4fd2368df68b7a7af0b7750f6936d7a1d829c087
AmanJain27 <ahjain2706@gmail.com>
2021-11-22 18:55:32 +0530

    Cleaned some part of the data

--- Data Cleaning/data_cleaning.py
+++ Data Cleaning/data_cleaning.py
@@ -15,15 +15,38 @@ def convert_csv(filepath):
 # to csv
 def tocsv(filepath, df):
     df.to_csv(filepath)
+    print('converted to csv')
 
 # clean the data
 def cleaning(dataframe):
-
+    
+    
     df = dataframe
-    df['Debut Year'] = df['Span'].split("-")[0]
-    df['Last Year'] = df['Span'].split("-")[1]
-    return df 
+    # debut year and final year
+    df['Debut Year'] = df['Span'].apply(lambda x: x.split('-')[0])
+    df['Last Year'] = df['Span'].apply(lambda x: x.split('-')[1])
+    # number of years active
+    df["Total Years"] = pd.to_numeric(df['Last Year']) - pd.to_numeric(df['Debut Year'])
+    # innings per matches
+    df['Innings per Matches'] = df['Inns']/df['Mat']
+    
+    # not outs per innings
+    df['Not outs per innings'] = df['NO']/df['Inns']
+    # average runs per innings (literal arithmatic average)
+    # Average in Cricket is calculated as (Total Runs) / (Inns played - Not out)
+    # Here average is (Total Runs) / (Inns played)
+    df['Arithmatic Average of Total Runs by Inns Played'] = df['Runs']/df['Inns']
+    # sort by number of not outs in desc order
+    df = df.sort_values("Not outs per innings", ascending=False)
+    return df
+
 
 
 def driver_code():
-    converting_path = ''
+    converting_path = '../Data Collection/scores.csv'
+    df = convert_csv(filepath=converting_path)
+    df = cleaning(df)
+    to_convert_path = 'scores_reference.csv'
+    tocsv(to_convert_path, df)
+
+driver_code()
